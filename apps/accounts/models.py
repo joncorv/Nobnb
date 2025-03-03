ba97c1e6bcd3995593f4d1f72d3efeb4ad800dec
Jonathan E. Corriveau <joncorv@gmail.com>
2025-03-02 20:50:15 -0800

    Make some updates to the models declarations for User and Listings

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-      pass # add fields here
+      city = models.CharField(max_length=100)
+      state_or_province = models.CharField(max_length=100)
+      country = models.CharField(max_length=100)
+      latitude = models.DecimalField(max_digits=9, decimal_places=6)
+      longitude = models.DecimalField(max_digits=9, decimal_places=6)
+      profile_picture = models.ImageField(upload_to='profile_pictures', null=True, blank=True)
--- apps/listings/models.py
+++ apps/listings/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from apps.accounts.models import User
 
 # Create your models here.
+class Listing(models.Model):
+	# General Facts about the Listing
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	city = models.CharField(max_length=100)
+	state_or_province = models.CharField(max_length=100)
+	country = models.CharField(max_length=100)
+	latitude = models.DecimalField(max_digits=9, decimal_places=6)
+	longitude = models.DecimalField(max_digits=9, decimal_places=6)
+	villain = models.CharField(max_length=100)
+	what_book_movie_show = models.CharField(max_length=100)
+	num_deaths = models.IntegerField()
+	# User and Creation Info
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{self.name} from {self.what_book_movie_show} in {self.city}, {self.state_or_province}"
+
+class Review(models.Model):
+	# Each review points to a single Listing
+	listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+	# User info
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	# Review data
+	body = models.TextField()
+	rating = models.IntegerField()
+	date_visited = models.DateTimeField()
+	was_murdered = models.BooleanField()
+	murders_witnessed = models.IntegerField()
+	does_recommend = models.BooleanField()
